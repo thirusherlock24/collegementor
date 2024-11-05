d3d04e1bcc8a2bdfd8934a44e5a73cff8d07eddf
thiru <thiru.loganathan@dreamsanddegrees.com>
2024-11-05 17:46:09 +0530

    sumne

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -2,9 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import blogs from "./screens/blogs/blogs-landing/BlogsAndArticlesMain"
+import BlogsAndArticlesMain from "./screens/blogs/blogs-landing/BlogsAndArticlesMain"
 // Type definition for Drawer Navigator
 export type RootDrawerParamList = {
   blogs: undefined;
@@ -16,7 +14,7 @@ const Navigation: React.FC = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="blogs" component={blogs} />
+        <Drawer.Screen name="blogs" component={BlogsAndArticlesMain} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
